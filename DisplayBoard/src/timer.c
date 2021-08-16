840432215bdcc895ec960e4851ded9caaffacd43
abdulWahab-tworks <abdul@tworks.in>
2021-08-16 15:27:23 +0530

    Exact 1msecs timer interrupt achieved by adjusting timer register and pre-scaler values and the same is verified on DSO.

--- DisplayBoard/src/timer.c
+++ DisplayBoard/src/timer.c
@@ -37,6 +37,7 @@
 #include <xc.h> 
 #include<avr/io.h>
 #include<avr/interrupt.h>
+#include <util/delay.h>
 
 // include project files - #include "" ----------------------------------------
 #include "../inc/timer.h"
@@ -44,8 +45,38 @@
 
 
 // '#' defines ----------------------------------------------------------------
-#define LED     PA4
+#define LED     PA6
+//  4 msecs approx.
+//#define SYSTICK_TIMER_COUNT         (63974)
+//#define SYSTICK_TIMER_PRESCALER     (1 << CS10)  // Timer mode with 1 no pre-scaling
 
+//// 1. came down to 3usecs, Freq = 163kHz
+//#define SYSTICK_TIMER_COUNT         (65535)
+//#define SYSTICK_TIMER_PRESCALER     (1 << CS10)  // Timer mode with 1 no pre-scaling
+
+//// 2. came down to 17usecs, Freq = 30kHz
+//#define SYSTICK_TIMER_COUNT         (65535)
+//#define SYSTICK_TIMER_PRESCALER     ((1 << CS12) | (0 << CS11) | (0 << CS10))  // Timer mode with 1 no pre-scaling
+
+//// 3. came down to 65usecs, Freq = 7kHz
+//#define SYSTICK_TIMER_COUNT         (65535)
+//#define SYSTICK_TIMER_PRESCALER     ((1 << CS12) | (0 << CS11) | (1 << CS10))  // Timer mode with 1 no pre-scaling
+
+//// 4. came down to 3 Secs, Freq = 131.6mHz
+//#define SYSTICK_TIMER_COUNT         (55530)
+//#define SYSTICK_TIMER_PRESCALER     ((1 << CS12) | (0 << CS11) | (1 << CS10))  // Timer mode with 1 no pre-scaling
+
+////// 5. came down to 650 msecs, Freq = 781.3mHz
+////#define SYSTICK_TIMER_COUNT         (55000)
+////#define SYSTICK_TIMER_PRESCALER     ((1 << CS12) | (0 << CS11) | (1 << CS10))  // Timer mode with 1 no pre-scaling
+
+//// 6. came down to 640 usecs, Freq = 757.3Hz
+//#define SYSTICK_TIMER_COUNT         (55000)
+//#define SYSTICK_TIMER_PRESCALER     ((0 << CS12) | (0 << CS11) | (1 << CS10))  // Timer mode with 1 no pre-scaling
+
+// 7. came down to 1.000 msecs, Freq = 1.000kHz
+#define SYSTICK_TIMER_COUNT         (51001)
+#define SYSTICK_TIMER_PRESCALER     ((0 << CS12) | (0 << CS11) | (1 << CS10))  // Timer mode with 1 no pre-scaling
 
 // 'Macros' -------------------------------------------------------------------
 																			  
@@ -76,8 +107,25 @@ extern "C" {
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ISR (TIMER1_OVF_vect)    // Timer1 ISR
 {
-	PORTA ^= (1 << LED);	
-	TCNT1 = 63974;   // for 1 sec at 16 MHz
+	//PORTA ^= (1 << LED);		
+    
+    PORTA |= (1 << LED);	
+    
+    volatile unsigned int i = 100;
+    while (i--)
+        ;
+    
+    PORTA &= ~(1 << LED);	
+
+	// TCNT1 = 63974;   // for 1 sec at 16 MHz
+    // TCNT1 = 639;
+    TCNT1 = SYSTICK_TIMER_COUNT;
+            
+    // Clear Interrupt flag..
+    TIFR1 &= ~(1 << TOV1);
+    
+    
+    
 }
 
 
@@ -102,10 +150,12 @@ void systick_timer_init (void)  {
 
     DDRD = (0x01 << LED);     //Configure the PORTD4 as output
 	
-	TCNT1 = 63974;   // for 1 sec at 16 MHz	
+	// TCNT1 = 63974;   // for 1 sec at 16 MHz	
+	TCNT1 = SYSTICK_TIMER_COUNT;
 
 	TCCR1A = 0x00;
-	TCCR1B = (1 << CS10) | (1 << CS12);;  // Timer mode with 1024 prescler
+    //	TCCR1B = (1 << CS10) | (1 << CS12);;  // Timer mode with 1024 prescler
+	TCCR1B = SYSTICK_TIMER_PRESCALER;
 	TIMSK1 = (1 << TOIE1) ;   // Enable timer1 overflow interrupt(TOIE1)
 	sei();        // Enable global interrupts by setting global interrupt enable bit in SREG
     
