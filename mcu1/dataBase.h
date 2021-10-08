f49ce282eb79b14abb3bb42c12fc9b4ebd010f15
abdulWahab-tworks <abdul@tworks.in>
2021-10-08 12:15:13 +0530

    Code re-shuffled and moved into dataBase.c & dataBase.h files for ease of handling.

---
+++ mcu1/dataBase.h
@@ -0,0 +1,29 @@
+// dataBase.h
+
+
+
+
+#ifndef _DATABASE_h
+#define _DATABASE_h
+
+
+#include "Arduino.h"
+#include "BoardDefines.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+
+// function prototypes..
+void pick_stroke_length (void);
+
+
+
+
+
+#endif
+
